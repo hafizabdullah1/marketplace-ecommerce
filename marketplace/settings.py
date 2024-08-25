557dc811d948f41a2c006f8206f11a158aab6a78
Abdullah2723 <127143084+Abdullah2723@users.noreply.github.com>
2024-08-25 18:42:34 +0500

    Make some changes to test  media files served in production or not

--- marketplace/settings.py
+++ marketplace/settings.py
@@ -138,7 +138,7 @@ import os
 
 STATIC_URL = 'static/'
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
